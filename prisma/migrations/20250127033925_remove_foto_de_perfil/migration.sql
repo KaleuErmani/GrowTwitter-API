44a05b01c1307f27e93ff8c847bdc17fc64ba1e6
kaleu ermane <kaleuborbapio1@gmail.com>
2025-01-27 00:39:42 -0300

    ..

---
+++ prisma/migrations/20250127033925_remove_foto_de_perfil/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `imagem_de_perfil` on the `users` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "users" DROP COLUMN "imagem_de_perfil";
